9c7789c8dd294206a879e5f7b618872fc9238f38
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2021-02-04 11:17:39 -0500

    Partially add LED tracking and function to set all LEDs to the same color

--- pyLED.py
+++ pyLED.py
@@ -1,9 +1,17 @@
 import serial
 import time
+import colorsys
+
+class LED:
+    def __init__(self, red = 0, blue = 0, green = 0):
+        self.red, self.blue, self.green  = red, blue, green
+        self.hue, self.saturation, self.value = colorsys.rgb_to_hsv(red, blue, green)
+    #Todo: keep HSV and RGB syncronized on writes
 
 class LedStrip:
     def __init__(self, port_name, led_count, baud=115200):
         self.num_led = led_count
+        self.LED_data = [LED(255, 0, 0)] * led_count
         try:
             self.serial_con = serial.Serial(port=port_name, baudrate=baud, timeout=1)
             if self.serial_con.is_open:
@@ -14,7 +22,6 @@ class LedStrip:
                 barry.append(byte2)
                 self.serial_con.write(barry)
                 result = self.serial_con.readline()
-                print(result)
                 if int(result) == led_count:
                     print("Serial conection established and verified")
                     return
@@ -48,14 +55,19 @@ class LedStrip:
     def draw(self):
         self.send_command(1, 0, 0, 0, 0)
         self.serial_con.readline() #Wait for confirmation from the arduino that the op is complete
+        
+    def set_RGB_all(self, r, g, b):
+        for led in range(self.num_led):
+            self.LED_data[led].red  = r
+            self.LED_data[led].green  = g
+            self.LED_data[led].blue  = b
+        self.send_command(2, 0, r, g, b)
     
 arduino = LedStrip("/dev/ttyACM0", 110)
 while True:
     for value in range(0,255,1):
-        #arduino.send_command(3, 0, 110, value, 1)
-        arduino.send_command(2, 0, int(value/2), 0, value)
+        arduino.set_RGB_all(int(value/2), 0, value)
         arduino.draw()
     for value in range(255,0,-1):
-        #arduino.send_command(3, 0, 110, value, 1)
-        arduino.send_command(2, 0, int(value/2), 0, value)
+        arduino.set_RGB_all(int(value/2), 0, value)
         arduino.draw()
